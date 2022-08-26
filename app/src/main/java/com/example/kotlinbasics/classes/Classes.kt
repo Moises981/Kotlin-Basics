e4da18e85526d4bde505dee2c788593b28b17773
Moises981 <jesus.castillo.j@tecsup.edu.pe>
2022-08-25 21:38:21 -0500

    Data class

--- app/src/main/java/com/example/kotlinbasics/classes/Classes.kt
+++ app/src/main/java/com/example/kotlinbasics/classes/Classes.kt
@@ -7,7 +7,7 @@ fun main() {
     val phone: Phone = Phone(241014124)
     phone.call()
     phone.showNumber()
-    println(phone.number)
+//    println(phone.number)
 
     newTopic("Inheritance")
     val smartphone: Smartphone = Smartphone(214150123, true);
@@ -16,4 +16,8 @@ fun main() {
     newTopic("Overloading")
     smartphone.showNumber()
     println("Private? ${smartphone.isPrivate}")
+
+    val myUser: User = User(0, "Jesus", "Castillo", 0)
+    println(myUser.component3())
+    println(myUser)
 }
---
+++ app/src/main/java/com/example/kotlinbasics/classes/User.kt
@@ -0,0 +1,4 @@
+package com.example.kotlinbasics.classes
+
+data class User(val id: Long, var name: String, var lastName: String, var group: Int) {
+}
