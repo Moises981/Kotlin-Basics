72aa67bbf31309bccbd77cf4a56943ee6141fdf5
Moises981 <jesus.castillo.j@tecsup.edu.pe>
2022-08-25 21:54:59 -0500

    Mutable list of

--- app/src/main/java/com/example/kotlinbasics/classes/Collections.kt
+++ app/src/main/java/com/example/kotlinbasics/classes/Collections.kt
@@ -10,5 +10,28 @@ fun main() {
     println(fruitList[fruitList.indices.random()])
     println("Index of grape is ${fruitList.indexOf("Grape")}")
 
+    newTopic("Mutable List")
 
+    val myUser: User = User(0, "Jesus", "Castillo", Group.FAMILY.ordinal)
+    val mySister: User = myUser.copy(id = 1, name = "Milagros")
+    val myFriend: User = myUser.copy(id = 2, group = Group.FRIEND.ordinal)
+
+    val usersList = mutableListOf<User>(myUser, mySister)
+    println(usersList)
+    usersList.add(myFriend)
+    println(usersList)
+//    usersList.removeAt(1)
+    usersList.remove(mySister);
+    println(usersList)
+
+    val usersSelectedList = mutableListOf<User>()
+    println(usersSelectedList)
+//    usersSelectedList.addAll(usersList);
+    usersSelectedList.add(myUser)
+    println(usersSelectedList)
+    usersSelectedList[0] = mySister
+    println(usersSelectedList)
+    usersSelectedList.add(myUser)
+    usersSelectedList.add(myUser)
+    println(usersSelectedList)
 }
